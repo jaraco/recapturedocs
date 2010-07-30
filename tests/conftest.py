fc17b7acd1a2622082f59a5981fb652b28f391fa
Jason R. Coombs <jaraco@jaraco.com>
2010-07-30 00:24:43 -0400

    Replaced receipt with lorem ipsum

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,4 +2,4 @@ import pkg_resources
 
 def pytest_funcarg__sample_stream(request):
 	req = pkg_resources.Requirement.parse('recapturedocs')
-	return pkg_resources.resource_stream(req, 'tests/sample.pdf')
+	return pkg_resources.resource_stream(req, 'tests/Lorem ipsum.pdf')
