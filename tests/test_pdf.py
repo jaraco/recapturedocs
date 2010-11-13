2c8148954c7c73405c0a9ef8189850eb55f03afe
Jason R. Coombs <jaraco@jaraco.com>
2010-11-13 13:11:26 -0500

    Fixed failing test_pdf test

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -5,4 +5,4 @@ from recapturedocs.turk import ConversionJob
 here = os.path.dirname(__file__)
 
 def test_split_pdf(sample_stream):
-	files = ConversionJob.split_pdf(sample_stream, 'sample.pdf')
+	files = ConversionJob.split_pdf(sample_stream)
