ef5a28e2cab6770dbf21e3eaa5fb6ab1f42e8d0d
Jason R. Coombs <jaraco@jaraco.com>
2010-11-13 13:12:30 -0500

    Fixed failing test_persistence test

--- tests/test_persistence.py
+++ tests/test_persistence.py
@@ -3,7 +3,5 @@ import pickle
 
 def test_persist_ConversionJob(sample_stream):
 	job = ConversionJob(sample_stream, content_type='application/pdf', server_url=None)
-	# a job cannot be pickled before the PDF has been processed
-	job.do_split_pdf()
 	reconstituted = pickle.loads(pickle.dumps(job))
  
