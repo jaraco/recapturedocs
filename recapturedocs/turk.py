ddb915622b0d92cf28b3212a4feb5fd724ac418f
Jason R. Coombs <jaraco@jaraco.com>
2011-05-27 15:12:43 -0400

    Refined the freelancer ad

--- recapturedocs/turk.py
+++ recapturedocs/turk.py
@@ -72,11 +72,20 @@ As you'll see when you go through it, the site is functional. A skeleton of cont
 
 2. The master template should include a compelling logo related to the service (document retyping). The logo must include the original vector image (preferred) or high-resolution raster image.
 
-3. One page design, the home page (based on the master template). The home page should include (inline, or link, or popup) a visualization of what the project is about. The visualization could be an intro video or static image or animated cartoon. Use your imagination here, but try to keep it classy. Finally, the home page should include a friendly, preferably animated form to upload the document and get started. I imagine the upload form overlay the main content after the user clicks a "Get Started" button.
+3. One page design, the home page (based on the master template). The home page should include (inline, or link, or popup) a visualization of what the project is about. The visualization could be an intro video or static image or animated cartoon. Use your imagination here, but try to keep it classy. Finally, the home page should include a friendly, preferably animated form to upload the document and get started. I imagine the upload form to overlay the main content after the user clicks a "Get Started" button.
 
-5. All pages should use well-structured XHTML and CSS and cross-browser javascript (must run on IE9, FF 3.5, and Chrome). jQuery is preferred for scripting. If another library is to be considered, you must provide a compelling reason why it should be used and I must approve the decision before accepting the bid.
+5. All pages should use well-structured XHTML and CSS and cross-browser javascript (must run on IE9, FF 3.5, and Chrome). jQuery is preferred for scripting. If another library is to be considered, you must provide a compelling reason why it should be used and I must approve the decision before accepting the bid. Provider is encouraged to use modern standards including HTML5 and web fonts. Provider is discouraged from using old techniques such as using images for buttons and rounded-corners.
+
+The project will be split into three equal milestones.
+
+1. In the first milestone, the provider will clarify any questions and provide a complete but draft deliverable of all components.
+
+2. I will give feedback and the provider will make minor adjustments and provide a final draft of all deliverable components.
+
+3. The final milestone is reserved for integration support and unforseen work such as necessary additional pages, enhancements, or tweaks within the general scope outlined above. This milestone will be complete when the deliverable has been sucessfully integrated with the existing application.
 
 Please supply portfolio references for similar work that includes site design, some dynamic behavior, and logo design.
+
 """
 
 log = logging.getLogger(__name__)
