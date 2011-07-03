e7adce967874bb5f78988ee38e2dce8d02733570
Jason R. Coombs <jaraco@jaraco.com>
2011-07-03 05:58:01 -0400

    Started work on a fabric script to capture deployment details

---
+++ fabfile.py
@@ -0,0 +1,22 @@
+from fabric.operations import sudo, run
+from fabric.contrib import files
+
+import yg.deploy.fabric.python
+
+def install_ppa_python():
+	yg.deploy.fabric.python.install_ppa_python()
+	sudo('aptitude install python2.7-dev')
+	sudo('aptitude install python-distribute-deadsnakes')
+	sudo('/usr/bin/easy_install-2.7 virtualenv')
+
+def create_user():
+	"Create a user under which recapturedocs will run"
+	#sudo('adduser --system --disabled-password --no-create-home recapturedocs')
+	#sudo('mkdir -m 700 -p ~recapturedocs/.ssh')
+	#files.append('~recapturedocs/.ssh/authorized_keys', [jaraco_pub], use_sudo=True)
+	#sudo('chown -R recapturedocs:nogroup ~recapturedocs/.ssh')
+
+def install_env():
+	sudo('virtualenv --no-site-packages /recapturedocs')
+	# requires libcap2-bin
+	#sudo('setcap "cap_net_bind_service=+ep" /recapturedocs/bin/python')
