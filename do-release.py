c3f3262addf284d02b29ff5c95e79a5173b7d183
Jason R. Coombs <jaraco@jaraco.com>
2011-11-26 12:26:16 -0500

    Updated do-release to use dropbox-index package

--- do-release.py
+++ do-release.py
@@ -2,10 +2,15 @@ import subprocess
 import os
 import sys
 
+import pkg_resources
+
+pkg_resources.require('dropbox-index>=0.4.3')
+import dropbox_index
+
 target = r'C:\Users\jaraco\Dropbox\public\cheeseshop'
 proc = subprocess.Popen([
 	sys.executable, 'setup.py', 'sdist',
 	'--dist-dir', target,
 	], stdout=subprocess.PIPE, stderr=open(os.path.devnull, 'w'))
 stdout, stderr = proc.communicate()
-subprocess.Popen(['dropbox-index', target]).wait()
+dropbox_index.crawl(target)
