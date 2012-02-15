9b34d13ceb5fbb6d5221fc351a2e02c5f44a51eb
Jason R. Coombs <jaraco@jaraco.com>
2012-02-15 13:42:57 -0700

    Added support for adding the MongoHQ addon

--- configure-heroku.py
+++ configure-heroku.py
@@ -21,6 +21,14 @@ class FixedUserKeyringPasswordManager(keyring.http.PasswordMgr):
 		# this call will only succeed on WinVault for now
 		keyring.get_keyring().delete_password(realm, user)
 
+def install_opener():
+	auth_manager = FixedUserKeyringPasswordManager(username='')
+	auth_handler = urllib2.HTTPBasicAuthHandler(auth_manager)
+	# build a new opener
+	opener = urllib2.build_opener(auth_handler)
+	# install it
+	urllib2.install_opener(opener)
+
 
 def configure_AWS():
 	access_key = '0ZWJV1BMM1Q6GXJ9J2G2'
@@ -33,12 +41,7 @@ def configure_AWS():
 		AWS_ACCESS_KEY_ID = access_key,
 		AWS_SECRET_ACCESS_KEY = secret_key,
 	)
-	auth_manager = FixedUserKeyringPasswordManager(username='')
-	auth_handler = urllib2.HTTPBasicAuthHandler(auth_manager)
-	# build a new opener
-	opener = urllib2.build_opener(auth_handler)
-	# install it
-	urllib2.install_opener(opener)
+	install_opener()
 
 	req = jaraco.net.http.MethodRequest(
 		url='https://api.heroku.com/apps/recapturedocs/config_vars',
@@ -48,5 +51,19 @@ def configure_AWS():
 	assert res.code == 200
 	pprint.pprint(json.loads(res.read()))
 
+def add_MongoHQ():
+	headers = {
+		'Accept': 'application/json',
+	}
+	req = jaraco.net.http.MethodRequest(
+		url = 'https://api.heroku.com/apps/recapturedocs/addons/mongohq',
+		method = 'POST', headers=headers,
+	)
+	install_opener()
+	res = urllib2.urlopen(req)
+	assert res.code == 200
+	pprint.pprint(json.loads(res.read()))
+
 if __name__ == '__main__':
 	configure_AWS()
+	add_MongoHQ()
