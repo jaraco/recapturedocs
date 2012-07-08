db41e8535bb940e45a079ae41a66161daef9e1a6
Jason R. Coombs <jaraco@jaraco.com>
2012-07-08 15:00:09 -0400

    Added test to exercise hit registration

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,7 +1,13 @@
-import dingus
+from recapturedocs import model
+from recapturedocs import aws
 
-def DingusTestCase(*args, **kwargs):
-	"""
-	A pytest-compatible DingusTestCase
-	"""
+class TestRetypePageHIT(object):
+	@classmethod
+	def setup_class(cls):
+		aws.set_connection_environment()
+		aws.ConnectionFactory.production = False
 
+	def test_register(self):
+		hit = model.RetypePageHIT('http://localhost/foo')
+		hit.register()
+		assert hasattr(hit, 'registration_result')
