e94fcb0ed14067e61cdc1486d32ea4d91ecd8bf1
Jason R. Coombs <jaraco@jaraco.com>
2012-09-15 08:42:41 -0400

    Updated dropbox routines

--- recapturedocs/dropbox.py
+++ recapturedocs/dropbox.py
@@ -1,29 +1,27 @@
 from __future__ import absolute_import, print_function
 
-import os
-
 import keyring
 import dropbox
 
 def get_session(access_key='ld83qebudvbirmj'):
-	if 'Dropbox secret' in os.environ:
-		return
 	secret = keyring.get_password('Dropbox RecaptureDocs', access_key)
 	assert secret, "Dropbox secret is null"
-	os.environ['Dropbox access key'] = access_key
-	os.environ['Dropbox secret'] = secret
 	return dropbox.session.DropboxSession(access_key, secret, 'app_folder')
+
+def get_client(sess):
+	return dropbox.client.DropboxClient(sess)
 
 def test_session():
 	sess = get_session()
 	request_token = sess.obtain_request_token()
-	url = sess.build_authorize_url(request_token)
+	callback = 'http://www.recapturedocs.com/...'
+	url = sess.build_authorize_url(request_token, oauth_callback=callback)
 	print("url:", url)
 	print("Please 'allow' this app at the above URL")
 	raw_input()
 
 	access_token = sess.obtain_access_token(request_token)
-	client = dropbox.client.DropboxClient(sess)
+	client = get_client(sess)
 	print("linked account:", client.account_info())
 
 	folder_metadata = client.metadata('/')
