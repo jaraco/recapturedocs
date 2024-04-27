6034929282c4b792e86c923eaad4ef2a259f1f0b
Jason R. Coombs <jaraco@jaraco.com>
2024-04-27 11:22:53 -0400

    Remove patching of boto and hope it doesn't apply to boto3.

--- recapturedocs/persistence.py
+++ recapturedocs/persistence.py
@@ -2,7 +2,6 @@ import cherrypy
 from jaraco.mongodb import helper
 
 from . import jsonpickle
-from .config import get_config_dir
 
 
 def init_mongodb():
@@ -16,24 +15,4 @@ def init_mongodb():
 
 def init():
     init_mongodb()
-    patch_boto_config()
     jsonpickle.setup_handlers()
-
-
-def patch_boto_config():
-    """
-    boto has a boto.config object which is used to persist boto
-    settings. The FPS module abuses this config by saving config to
-    system locations. This function patches the config so that config
-    data is only ever written to the recapturedocs config location.
-    """
-    import boto.pyami.config
-
-    config_file = get_config_dir() / 'boto.cfg'
-    boto.pyami.config.UserConfigPath = config_file
-    boto.pyami.config.BotoConfigLocations = [config_file]
-    # set the system config path to an invalid name so nothing is ever
-    #  stored there
-    boto.pyami.config.BotoConfigPath = '/invalid/path/name'
-    # recreate the boto.config instance
-    boto.config = boto.pyami.config.Config()
